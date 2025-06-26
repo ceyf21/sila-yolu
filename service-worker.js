7483257d174936513221ba0f22a3c7d07dc09b11
ceyf21 <ceyf@yahoo.com>
2025-06-26 21:34:00 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,21 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('sila-yolu-cache-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/icon.png',
+        '/manifest.json'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', event => {
-  self.clients.claim();
-});
-
-// Basic fetch handler (network first)
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
